0691209648bfc907bfa2e7b8ea8d4bc335c258b5
Robert Wildman <robertwildman4@gmail.com>
2017-01-25 15:21:12 +0000

    Adding the lang saver thanks scott

--- index.js
+++ index.js
@@ -16,8 +16,14 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
+var connector2 = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
 var bot = new builder.UniversalBot(connector);
+var bot1 = new builder.UniversalBot(connector2);
 server.post('/api/messages', connector.listen());
+server.get('/api/messages', connector2.listen());
 
 //=========================================================
 // Bots Dialogs
@@ -33,27 +39,46 @@ const translateClient = Translate({
   projectId: projectId,
   keyFilename: 'Translator-131a6896fcd5.json'
 }); 
-
+bot1.dialog('/',function(session) {
+	session.send('Hello');
+})
 
 bot.dialog('/', function (session) {
+	var userlan = session.userData.Language;
 	var text = session.message.text;
-    translateClient.detect(session.message.text, function(err, results) {
-  	if (!err) {
-  		console.log(results);
-  		var lan = results.language;
-    	//Decacts the code and keeps if its not the user lanagues
-    	if(results.language != 'en')
-    	{
-			translateClient.translate(session.message.text, 'en').then((results) => {
-   			const translation = results[0];
-   			session.send(translation + ' translated from ' + lan);  });
-    	}
-  	}
-  	else
-  	{
-  		console.log(err);
-    	session.send(err);
-  	}
-	});
-});
+	 if (!session.userData.Language || text == 'edit lang') {
+            session.beginDialog('/profile');
+      } else {
+    	translateClient.detect(session.message.text, function(err, results) {
+  			if (!err) {
+  			console.log(results);
+  			var lan = results.language;
+    		//Decacts the code and keeps if its not the user lanagues
+    		if(results.language != userlan)
+    		{
+				translateClient.translate(session.message.text, userlan).then((results) => {
+   				const translation = results[0];
+   				session.send(translation + ' translated from ' + lan);
+   				});
+    		}
+  			else
+  			{
+  				console.log(err);
+    			session.send(err);
+  			}
+  			}
+     	});
+    }
+   });
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Please select your language; (en / es / fr / de / pt)');
+    },
+    function (session, results) {
+        session.userData.Language = results.response;
+        session.send('Language is now ' + results.response); 
+        session.endDialog();
+    }
+]);
 
